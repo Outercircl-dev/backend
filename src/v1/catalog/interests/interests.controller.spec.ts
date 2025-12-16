0ea72f85a12c0dde02c14b2289c5a941c72ea246
Harsh Gupta <harshrp94@gmail.com>
2025-12-16 16:40:34 +0000

    prisma client mock issues on github

--- src/v1/catalog/interests/interests.controller.spec.ts
+++ src/v1/catalog/interests/interests.controller.spec.ts
@@ -2,14 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { InterestsController } from './interests.controller';
 import { InterestsService } from './interests.service';
 
-jest.mock('src/generated/prisma/client', () => {
-  class PrismaClient {
-    interest = { findMany: jest.fn() };
-    $disconnect = jest.fn();
-    $connect = jest.fn();
-  }
-  return { PrismaClient };
-}, { virtual: true });
+jest.mock('src/generated/prisma/client');
 
 describe('InterestsController', () => {
   let controller: InterestsController;
