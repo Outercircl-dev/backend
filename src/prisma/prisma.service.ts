9d8226ea2a80def14abb21a040d3c4fa0b1b0de2
Harsh Gupta <harshrp94@gmail.com>
2025-12-16 16:46:24 +0000

    prisma client mock issues on github

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,8 +6,7 @@ import { PrismaClient } from 'src/generated/prisma/client';
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+  implements OnModuleInit, OnModuleDestroy {
   constructor(private readonly config: ConfigService) {
     const adapter = new PrismaPg({
       connectionString: config.getOrThrow('database.url'),
